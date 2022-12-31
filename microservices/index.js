e850ab64c7f41ee57e0ed296fecf2816544f42f8
cyoung43 <55853741+cyoung43@users.noreply.github.com>
2022-12-30 20:05:14 -0700

    restructuring and simplifying for testing purposes

--- microservices/index.js
+++ microservices/index.js
@@ -16,90 +16,37 @@ const HEALTH_PATH = '/health'
 const WORDS_PATH = '/words'
 const WORD_PATH = '/word'
 
-exports.handler = async function (event) {
+exports.handler = async function (event, context, callback) {
     console.log('Request event', event)
-    let response
+    
+    const request_body = JSON.parse(event.body)
 
-    switch (true) {
-        case event.httpMethod === 'GET' && event.path === HEALTH_PATH:
-            response = buildResponse(200)
-            break
-        case event.httpMethod === 'GET' && event.path === WORDS_PATH:
-            response = await getWords()
-            break
-        case event.httpMethod === 'GET' && event.path === WORD_PATH:
-            response = await getWord(event.queryStringParameters.word)
-            break
-        default:
-            response = buildResponse(404, '404 not found')
-    }
+    console.log('Request body', request_body)
 
-    return response
-}
-
-// Get all words
-const getWords = async () => {
-    const params = { TableName: TABLE }
-    const all_words = await scanDynamoDbRecords(params, [])
-    const body = {
-        words: all_words
-    }
-
-    return buildResponse(200, body)
-}
-
-const scanDynamoDbRecords = async (scan_params, item_array) => {
     try {
-        // Read Dynamo DB data, pushing into array
-        const data = await dynamo_db.scan(scan_params).promise()
-        item_array = [...item_array, ...data.Items]
+        const data = await get_word('ádlaw')
 
-        if (data.LastEvaluatedKey) {
-            scan_params.ExclusiveStartKey = data.LastEvaluatedKey
-            return await scanDynamoDbRecords(scan_params, item_array)
-        }
-
-        return item_array
+        return { body: JSON.stringify(data) }
     }
-
     catch (err) {
-        console.log('ERROR in scan DynamoDB records', err)
+        return { error: err }
     }
 }
 
-// Get specific word
-const getWord = async (word_id) => {
+const get_word = async (word, search) => {
     const params = {
         TableName: TABLE,
         Key: {
-            word: word_id
+            id: word
         }
     }
 
-    return await dynamo_db
-        .get(params)
-        .promise()
-        .then((response) => {
-            if (response.Item) {
-                return buildResponse(200, response.Item)
-            }
+    try {
+        const data = await dynamo_db.get(params).promise()
 
-            return buildResponse(404, `Word ${word_id} not found. Please try again`) 
-        }, (err) => {
-            console.log('ERROR', err)
-        })
-}
-
-// Response helper
-const buildResponse = (status_code, body) => {
-    const response = {
-        statusCode: status_code,
-        headers: {
-            'Content-type': 'application/json'
-        },
-        isBase64Encoded: false,
-        body: body
+        return data
     }
-
-    return JSON.stringify(response)
+    catch (err) {
+        return err
+    }
 }
