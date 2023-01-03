8bfff8de23dd3936814592aa7622f83c21bd36ab
cyoung43 <55853741+cyoung43@users.noreply.github.com>
2023-01-02 21:39:44 -0700

    restructure code to use v3 of sdk api

--- microservices/index.js
+++ microservices/index.js
@@ -1,19 +1,28 @@
 // AWS SDK config
 const AWS = require('aws-sdk')
 const AWS_REGION = 'us-east-1'
+
+// docs: https://github.com/aws/aws-sdk-js-v3/blob/main/lib/lib-dynamodb/README.md
+// imports
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import {
+    DynamoDBDocumentClient,
+    ScanCommand,
+    PutCommand,
+    GetCommand,
+    DeleteCommand,
+} from "@aws-sdk/lib-dynamodb"
+
 const TABLE = process.env.TABLE
 const KEY = process.env.PRIMARY_KEY
-
-AWS.config.update({
-    region: AWS_REGION
-})
 
 // TEST COMMENT FOR WORKFLOW
 // They use dynamodbclient in this API gateway tutorial vs the AWS.DynamoDB.DocumentClient()... why?
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/modules/_aws_sdk_lib_dynamodb.html
 
 // Constants
-const dynamo_db = new AWS.DynamoDB.DocumentClient()
+const client = new DynamoDBClient({ region: 'us-east-1' })
+const dynamo = DynamoDBDocumentClient.from(client)
 const HEALTH_PATH = '/health'
 const WORDS_PATH = '/words'
 const WORD_PATH = '/word'
@@ -29,7 +38,7 @@ exports.handler = async function (event, context, callback) {
         const data = await get_word(request_body)
         console.log('data', data)
 
-        return { body: JSON.stringify(data) }
+        return { body: JSON.stringify(data.item) }
     }
     catch (err) {
         return { error: err }
@@ -45,10 +54,10 @@ const get_word = async ({ word, sort }) => {
         }
     }
 
-    console.log('Params', params)
-
     try {
-        const data = await dynamo_db.get(params).promise()
+        const data = await dynamo.send(
+            new GetCommand(params)
+        )
 
         return data
     }
